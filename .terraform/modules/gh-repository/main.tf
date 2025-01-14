6c9a2fb8f5fd349370a889d8d14dd76e46e7f0db
tmslpm <thomaslepomme@gmail.com>
2025-01-14 21:08:54 +0100

    fix workflows

--- .terraform/modules/gh-repository/main.tf
+++ .terraform/modules/gh-repository/main.tf
@@ -62,10 +62,24 @@ resource "github_repository_file" "subproject_readme" {
   for_each            = var.github_repository_subprojects
   repository          = var.github_repository_name
   branch              = "main"
-  file                = "src/packages/${each.value}/readme.md"
+  file                = "src/packages/${each.value}/readme.terraform.md"
   content             = "# Managed by Terraform"
   commit_message      = "Managed by Terraform - subproject ${each.value}"
   commit_author       = "terraform"
   commit_email        = "noreply@terraform.com"
-  overwrite_on_create = false
+  overwrite_on_create = true
 }
+/*
+resource "github_issue_label" "example_labels" {
+  for_each = {
+    "bug"        = { color = "d73a4a", description = "Something is not working" }
+    "enhancement" = { color = "a2eeef", description = "New feature or request" }
+    "documentation" = { color = "0075ca", description = "Improvements or additions to documentation" }
+  }
+
+  repository  = var.github_repository_name
+  name        = each.key
+  color       = each.value.color
+  description = each.value.description
+}
+*/
